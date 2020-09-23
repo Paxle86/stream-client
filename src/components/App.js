5f8289f42948c60029554b3d527f7244c4c43fda
Thanh <thanh@Thanhs-MacBook-Pro.local>
2020-09-23 20:48:59 +0700

    seting up router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
+import StreamDelete from '../components/streams/StreamDelete';
+import Header from './Header';
 
 const App = () => {
 	return (
-		<div>
-			<h1>Appppppp</h1>
+		<div className='ui container'>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route path='/' exact component={StreamList} />
+					<Route path='/streams/new' exact component={StreamCreate} />
+					<Route path='/streams/edit' exact component={StreamEdit} />
+					<Route path='/streams/delete' exact component={StreamDelete} />
+					<Route path='/streams/show' exact component={StreamShow} />
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 };
